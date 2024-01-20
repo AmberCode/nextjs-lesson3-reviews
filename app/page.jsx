5ca76b2fa8ce75578a995e7af2738185cb7720c8
Andrius Urbelis <andrius.urbelis@coxautoinc.com>
2024-01-20 13:22:18 +0200

    default feature

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,27 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import { getFeatureReview } from "@/lib/review";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const defaultReview = await getFeatureReview();
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className="pb-3">Only the best indie games, reviewed for you</p>
       <div className="bg-white border rounded shadow w-80 md:w-full hover:shadow-xl">
         <Link
-          href="/reviews/stardew-valley"
+          href={`/reviews/${defaultReview.slug}`}
           className="flex flex-col sm:flex-row"
         >
           <img
-            src="/images/stardew-valley.jpg"
+            src={`${defaultReview.image}`}
             alt=""
             width="320"
             height="180"
             className="mb-2 rounded-t"
           />
           <h2 className="font-semibold font-orbitron py-1 text-center">
-            Stardew Valley
+            {defaultReview.title}
           </h2>
         </Link>
       </div>
